7e7e08165283f289106d631a018a374c9651afd8
aysha-razack <47174966+aysha-razack@users.noreply.github.com>
2019-02-12 12:12:53 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,6 +1,7 @@
-num = int(input( ))
-mod = num % 2
-if mod > 0:
-    print("ODD")
+ch = input("Enter a character: ")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+ or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+    print(ch, "is a Vowel")
 else:
-    print("EVEN")
+    print(ch, "is a Consonant")
